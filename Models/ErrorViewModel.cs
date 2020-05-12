1bda8a133bc7f21123ed33c5c54be9bf40c78b4a
John Green <johngreen8828@yahoo.com>
2020-05-12 12:29:46 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DietzelCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
